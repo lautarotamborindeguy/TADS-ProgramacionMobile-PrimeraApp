c683ffef34cc455f95371b542983fc725e0c4766
Lautaro Tamborindeguy <lautarotegui4@gmail.com>
2026-03-04 21:02:05 -0300

    App update, new funcinallities and styles

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# app1
+# flutter_application_1
 
 A new Flutter project.
--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,42 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("App 1"),
-          backgroundColor: Colors.lightBlue,
-          foregroundColor: Colors.white,
+          title: Text("App test"),
+          backgroundColor: Colors.amber,
+          foregroundColor: Colors.black,
           centerTitle: true,
         ),
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          margin: EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [Text("Contenido", style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.amber
+              )),
+              ElevatedButton(onPressed: (){
+                print("Hola mundo");
+              }, child: Text("Salvar")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("Row child 1", style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber,
+                  )),
+                  Text("Row child 2", style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber
+                  )),
+              ])
+            ]
+          ),
         ),
       ),
     );
